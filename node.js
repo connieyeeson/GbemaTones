606bea071ede05afb4e748a1e030eb84be00e3c4
connieyeeson <connieyeeson@gmail.com>
2023-09-07 03:58:23 +0000

    mobile view

---
+++ node.js
@@ -0,0 +1,7 @@
+
+// Target Elements
+
+const displayPlaylist = document.querySelector(".musicGenre");
+const playMusic = document.querySelector(".artistList");
+const searchMusic = document.querySelector(".searchMusic");
+const 
